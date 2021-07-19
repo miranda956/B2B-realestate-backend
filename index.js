761959569965ca5860e9d3a2f49823b33a6cb5c4
miranda956 <musularonald17@gmail.com>
2021-07-19 21:59:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express =require("express");
+const bodyparser =require("body-parser");
+const methodoverride =require("method-override");
+const path =require("path");
+const winston =require("winston")
+const app =express();
+const  expresswinston =require("express-winston");
+
+app.use(bodyparser.json());
+app.use(methodoverride());
+app.use(expresswinston.logger({
+    transports:[
+        new winston.transports.Console({
+            // @ts-ignore
+            json:true,
+            colirize:true
+        }),
+        new winston.transports.File({
+            filename:'logs/success.log'
+        })
+    ]
+}))
+app.use(expresswinston.errorLogger({
+    transports:[
+        new winston.transports.Console({
+            // @ts-ignore
+            json:true,
+            colorize:true
+        }),
+        new winston.transports.File({
+            filename:"logs/error.log"
+        })
+    ]
+}));
+ app.listen(6050, function(req, res) {
+        console.log('server listening to localhost 6050');
+      });
